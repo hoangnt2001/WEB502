dd9efb94c5ba6ea8376cb287e367303f1c73fbf0
hoangnt2001 <congtuhonguyen2001lg@gmail.com>
2022-03-11 20:18:08 +0700

    study

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,33 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
+const person = {
+  name: "hoang",
+  age: 20,
+  status: true,
+  products: [
+    { id: "1", name: "productA"},
+    { id: "2", name: "productB"},
+  ]
+}
+// Sử dụng hàm trong react
+const showAge = (age) => <p>Tuoi cua ban la: {age}</p> // 20
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+// Sử dụng component trong react
+
+const ShowAge = props => <p>Tuoi cua ban la: {props.age}</p> // { age: 20, name: "Dat" }
+
+ReactDOM.render(<div>
+  <p>Ten: {person.name} </p>
+  <p>Tuoi: {person.age} </p>
+  <p>{person.status ? "Da ket hon" : "Chua ket hon"}</p>
+  <ul>
+    {person.products.map(product => <li>
+      {product.name}
+      {product.id}
+
+    </li>)}
+  </ul>
+  <div>function: { showAge(person.age) }</div>
+  <div>Component: <ShowAge age={person.age} name="Dat" /></div>
+</div>, document.querySelector('#root'));
